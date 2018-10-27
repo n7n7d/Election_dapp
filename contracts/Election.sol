174d8ea69334558cd37ce3f50e9b3b34c6ac6a24
Nischay namdev <nischay007@gmail.com>
2018-10-27 12:24:47 +0530

    Creating first contract

---
+++ contracts/Election.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.24;
+
+contract Election{
+  //store candidate
+  //read candidate
+  //constructor
+
+  string public candidate;
+
+  function Election() public{
+    candidate = "Candidate 1";
+  }
+
+}
